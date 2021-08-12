e27e2a9cc7589071c42fdc4f99876a29e14e5bb3
supun-priyashan <68734943+supun-priyashan@users.noreply.github.com>
2021-08-12 15:34:21 +0530

    Implemented room service module and route module

--- server/services/room.service.js
+++ server/services/room.service.js
@@ -1,1 +1,39 @@
-//Service module for rooms in backend
+const Room = require('../models/room.model');
+
+const addRoom = async (request, response) => {
+
+    const room = request.body;
+
+    await room.save().
+        then((data) => {
+            response.status(200).
+            json({
+                success: true,
+                room: data
+            });
+    }).catch((error) => {
+        response.status(500).
+        json({
+            success: false,
+            error: error
+        });
+    })
+}
+
+const getRooms = async (request, response) => {
+    try{
+        const rooms = Room.find();
+        response.status(200).
+            json({
+            success: true,
+            rooms: rooms
+        })
+    } catch (error) {
+        response.status(404).json({ error: error });
+    }
+}
+
+module.exports = {
+    getRooms,
+    addRoom
+}
