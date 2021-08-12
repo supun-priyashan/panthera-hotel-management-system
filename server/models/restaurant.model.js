8a1f99e90946e52970bd891bfc9abfdd8bbae52a
DilithaR <ranjunadilitha@gmail.com>
2021-08-12 19:24:43 +0530

    model added.

--- server/models/restaurant.model.js
+++ server/models/restaurant.model.js
@@ -1,1 +1,8 @@
-//comment 33 456
+const mongoose = require('mongoose');
+
+const restaurantSchema = new mongoose.Schema({
+    restaurantName: {type: String, required: true, trim: true},
+    caption: {type: String, required: true, trim: true},
+    description: {type: String, required: true, trim: true},
+    images: {type: Array, default: []}
+});
