279115e12df46989d405c258b6b1e81604bfb6fe
supun-priyashan <68734943+supun-priyashan@users.noreply.github.com>
2021-08-16 22:26:24 +0530

    Got sidebar working

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-import SideNav from "./components/views/SideNav/SideNav";
-import {useState} from "react";
+import Sidebar from './components/views/SideNav/SideBar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Overview from './components/views/Overview';
+import { Reports, ReportsOne, ReportsTwo, ReportsThree } from './components/views/Reports';
+import Team from './components/views/Team';
 
 function App() {
-    const [sidebarOpen, setSidebarOpen] = useState(true);
-
     return (
-        <div className="App">
-            <div className="flex h-screen overflow-hidden">
-                <SideNav sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-            </div>
-        </div>
+        <Router>
+            <Sidebar />
+            <Switch>
+                <Route path='/overview' exact component={Overview} />
+                <Route path='/reports' exact component={Reports} />
+                <Route path='/reports/reports1' exact component={ReportsOne} />
+                <Route path='/reports/reports2' exact component={ReportsTwo} />
+                <Route path='/reports/reports3' exact component={ReportsThree} />
+                <Route path='/team' exact component={Team} />
+            </Switch>
+        </Router>
     );
 }
 
