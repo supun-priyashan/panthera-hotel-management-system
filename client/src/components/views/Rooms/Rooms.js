5d2c930cc0a07f0a0ec4cae4253b663cf03c34a6
supun-priyashan <68734943+supun-priyashan@users.noreply.github.com>
2021-08-17 22:30:24 +0530

    Added material table to admin panel

--- client/src/components/views/Rooms/Rooms.js
+++ client/src/components/views/Rooms/Rooms.js
@@ -1,12 +1,72 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
+import MaterialTable from "material-table";
+import {Fab} from "@material-ui/core";
 
 export const Rooms = () => {
+    const myTextIcon = React.useRef(null);
+
     return (
         <div className={'content'}>
             <div className={'dashboard-header'}>
                 Rooms & Suite Management
             </div>
             <div className={'main-container'}>
+                <div className={'table-container'}>
+                    <MaterialTable
+                        title="Rooms and Suites"
+                        columns={[
+                            { title: 'Name', field: 'name' },
+                            { title: 'Surname', field: 'surname' },
+                            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+                            {
+                                title: 'Birth Place',
+                                field: 'birthCity',
+                                lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+                            },
+                        ]}
+                        data={[
+                            { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                            { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+                            { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                            { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+                            { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                            { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+                            { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                            { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+                        ]}
+                        actions={[
+                            {
+                                icon: 'edit',
+                                tooltip: 'Edit User',
+                                onClick: (event, rowData) => alert("You saved " + rowData.name)
+                            },
+
+                            {
+                                icon: 'delete',
+                                tooltip: 'Delete User',
+                                onClick: (event, rowData) => alert("You want to delete " + rowData.name),
+                            },
+                            {
+                                icon: "add_box",
+                                tooltip: "Add new room",
+                                //position: "center",
+                                isFreeAction:true,
+                                onClick: () => {
+                                    console.log("clicked");
+                                }
+                            }
+                        ]}
+                        options={{
+                            actionsColumnIndex: -1,
+                            tableLayout: 'auto',
+                            exportButton: true,
+                            sorting: true,
+                            pageSize: 6,
+                            pageSizeOptions: [6],
+                            showTitle: false,
+                        }}
+                    />
+                </div>
             </div>
         </div>
     );
